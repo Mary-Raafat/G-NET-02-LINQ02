9eaffff3e3a3988ff1916171b2bb3a554e3afe69
Mary-Raafat <morotadrous@gmail.com>
2026-04-19 18:08:36 +0200

    Q2

--- G-NET-02-LINQ02/Program.cs
+++ G-NET-02-LINQ02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using G_NET_02_LINQ02.DataSources;
+using G_NET_02_LINQ02.Models;
 
 namespace G_NET_02_LINQ02
 {
@@ -7,11 +8,18 @@ namespace G_NET_02_LINQ02
         static void Main(string[] args)
         {
             #region Q1
-         //var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            //var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
+
+            #region Q2
+            //int pageNumber = 2;
+            //int pageSize = 5;
+            //var result = Source.ProductList.OrderBy(p => p.ProductID).Skip(((pageNumber - 1) * pageSize)).Take(pageSize);
+
             #endregion
 
 
-            foreach(var item in result)
+            foreach (var item in result)
             {
                 Console.WriteLine(item);
             }
