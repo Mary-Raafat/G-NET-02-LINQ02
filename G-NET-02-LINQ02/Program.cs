2f5fe79cb334e99c31e920088238ec68b3004a3f
Mary-Raafat <morotadrous@gmail.com>
2026-04-19 18:02:06 +0200

    Q1

--- G-NET-02-LINQ02/Program.cs
+++ G-NET-02-LINQ02/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using G_NET_02_LINQ02.DataSources;
+
+namespace G_NET_02_LINQ02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+         //var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            #endregion
+
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
